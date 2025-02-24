e3bac9e322e5dbc2547d9f369648718ad7b06121
Jackson Cowdrey <jackson.taylor.cowdrey@gmail.com>
2025-02-23 20:54:12 -0600

    update binary.rs

--- src/binary.rs
+++ src/binary.rs
@@ -1,18 +1,42 @@
-use goblin::Object;
+use goblin::elf::Elf;
 use std::fs::File;
 use std::io::{self, Read};
 
-pub fn get_binary_architecture(path: &str) -> io::Result<String> {
-    let mut file = File::open(path)?;
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer)?;
+// TODO: Relro status, PIE status, NX status, Stack Canary status, FORTIFY_SOURCE status, etc
 
-    let arch = match Object::parse(&buffer).unwrap() {
-        Object::Elf(elf) => format!("ELF - {:?}", elf.header.e_machine),
-        _ => "Unknown binary format".to_string(),
-    };
-
-    Ok(arch)
+#[derive(Debug)]
+pub struct ELFBinary {
+    pub path: String,
+    pub architecture: String,
+    pub relro: String,
+    pub pie: String,
+    pub nx: String,
+    pub canary: String,
+    pub fortify_source: String,
 }
 
-// TODO: Relro status, PIE status, NX status, Stack Canary status, FORTIFY_SOURCE status, etc
+impl ELFBinary {
+    pub fn architecture_from_file(path: &str) -> io::Result<Self> {
+        let mut file = File::open(path)?;
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer)?;
+
+        let elf = Elf::parse(&buffer).map_err(|e| io:Error::new(io::ErrorKind::InvalidData, e))?;
+
+        let architecture = match elf.header.e_machine {
+            goblin::elf::header::EM_X86_64 => "x86_64",
+            // ...
+        }
+        .to_string();
+
+        Ok(Self {
+            path: path.to_string(),
+            architecture,
+            relro,
+            pie,
+            nx,
+            canary,
+            fortify_source,
+        })
+    }
+}
